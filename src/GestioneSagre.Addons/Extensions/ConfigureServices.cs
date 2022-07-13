e44f116ecac04711312bdd858b89dda38e2c1f04
Angelo Pirola <angelo9871@gmail.com>
2022-07-13 22:53:02 +0200

    Aggiunto nuovo servizio

--- src/GestioneSagre.Addons/Extensions/ConfigureServices.cs
+++ src/GestioneSagre.Addons/Extensions/ConfigureServices.cs
@@ -3,6 +3,7 @@ using GestioneSagre.Email.Services.Interfaces;
 using GestioneSagre.Image.Services;
 using GestioneSagre.Image.Services.Interfaces;
 using GestioneSagre.Tools.TransactionLog;
+using GestioneSagre.Upload.UploadLogo;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,16 +13,18 @@ public static class ConfigureServices
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
-        // Services TRANSIENT
-        //services.Scan(scan => scan.FromAssemblyOf<EfCoreFestaService>()
-        //    .AddClasses(classes => classes.Where(type => type.Name.EndsWith("Service")))
-        //    .AsImplementedInterfaces()
-        //    .WithTransientLifetime());
+        // Services TRANSIENT - GestioneSagre.Upload
+        services.Scan(scan => scan.FromAssemblyOf<EfCoreLogoService>()
+            .AddClasses(classes => classes.Where(type => type.Name.EndsWith("Service")))
+            .AsImplementedInterfaces()
+            .WithTransientLifetime());
+
+        // Services TRANSIENT - GestioneSagre.Application
 
         // Services SINGLETON
-        services.AddSingleton<IEmailSenderService, MailKitEmailSender>();
-        services.AddSingleton<IImagePersister, MagickNetImagePersister>();
-        services.AddSingleton<ITransactionLogger, LocalTransactionLogger>();
+        services.AddSingleton<IEmailSenderService, MailKitEmailSender>();       //GestioneSagre.Email
+        services.AddSingleton<IImagePersister, MagickNetImagePersister>();      //GestioneSagre.Image
+        services.AddSingleton<ITransactionLogger, LocalTransactionLogger>();    //GestioneSagre.Tools
 
         return services;
     }
